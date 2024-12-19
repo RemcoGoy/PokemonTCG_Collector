cdab6915e9d97da6621ba9206a15e8e0dc63c1fb
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-12-19 09:26:46 +0100

    Started working on tst

--- backend/internal/server/auth.go
+++ backend/internal/server/auth.go
@@ -5,9 +5,15 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/supabase-community/gotrue-go/types"
 )
 
 type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RegisterRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
@@ -41,8 +47,41 @@ func (s *Server) Login(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(jsonResp)
 }
 
+func (s *Server) Signup(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]any)
+	var registerRequest RegisterRequest
+	err := json.NewDecoder(r.Body).Decode(&registerRequest)
+	if err != nil {
+		resp["error"] = err.Error()
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		user, err := s.SupabaseClient.Client.Auth.Signup(types.SignupRequest{
+			Email:    registerRequest.Email,
+			Password: registerRequest.Password,
+		})
+		if err != nil {
+			resp["error"] = err.Error()
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			resp["email"] = user.Email
+			w.Header().Set("Content-Type", "application/json")
+		}
+	}
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		resp["error"] = err.Error()
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
 func AuthRouter(s *Server) chi.Router {
 	r := chi.NewRouter()
 	r.Post("/login", s.Login)
+	r.Post("/signup", s.Signup)
 	return r
 }
