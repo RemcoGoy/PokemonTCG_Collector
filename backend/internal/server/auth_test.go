46639675557f1635b5bdced32978c3693395cd07
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-12-25 12:26:42 +0100

    Add login test

--- backend/internal/server/auth_test.go
+++ backend/internal/server/auth_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -83,5 +84,40 @@ func TestSignupFailedHandler(t *testing.T) {
 	}
 	if expected != string(body) {
 		t.Errorf("expected response body to be %v; got %v", expected, string(body))
+	}
+}
+
+func TestLoginHandler(t *testing.T) {
+	user_email := "remco.goy@hotmail.com"
+	user_password := "testpwd"
+
+	s := &Server{
+		SupabaseFactory: test.NewMockSupabaseFactory(),
+	}
+	server := httptest.NewServer(http.HandlerFunc(s.Login))
+	defer server.Close()
+
+	resp, err := http.Post(server.URL, "application/json", strings.NewReader(fmt.Sprintf("{\"email\":\"%s\",\"password\":\"%s\"}", user_email, user_password)))
+	if err != nil {
+		t.Fatalf("error making request to server. Err: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("error reading response body. Err: %v", err)
+	}
+	var response map[string]interface{}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		t.Fatalf("error unmarshaling response body: %v", err)
+	}
+
+	if _, ok := response["token"]; !ok {
+		t.Error("expected response to contain token key")
 	}
 }
