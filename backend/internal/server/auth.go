428e17a4168b30aa140a0f668f96ecfa03b17a0c
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-12-27 14:51:19 +0100

    Update signup call

--- backend/internal/server/auth.go
+++ backend/internal/server/auth.go
@@ -81,41 +81,40 @@ func (s *Server) Logout(w http.ResponseWriter, r *http.Request) {
 
 func (s *Server) Signup(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]any)
+
 	var registerRequest RegisterRequest
 	err := json.NewDecoder(r.Body).Decode(&registerRequest)
 	if err != nil {
-		resp["error"] = err.Error()
-		w.WriteHeader(http.StatusBadRequest)
-	} else {
-		user, err := s.SupabaseFactory.CreateClient().Auth.Signup(types.SignupRequest{
-			Email:    registerRequest.Email,
-			Password: registerRequest.Password,
-		})
-
-		if err != nil {
-			resp["error"] = err.Error()
-			w.WriteHeader(http.StatusBadRequest)
-		} else {
-			profile := t.Profile{
-				ID:       user.ID,
-				Username: registerRequest.Username,
-			}
-			_, _, err := s.SupabaseFactory.CreateAdminClient().From("profile").Insert(profile, true, "", "", "").Execute()
-			if err != nil {
-				resp["error"] = err.Error()
-
-				err = s.SupabaseFactory.CreateAdminClient().Auth.AdminDeleteUser(types.AdminDeleteUserRequest{UserID: user.ID})
-				if err != nil {
-					resp["error"] = err.Error()
-				}
-
-				w.WriteHeader(http.StatusBadRequest)
-			} else {
-				resp["email"] = user.Email
-				w.Header().Set("Content-Type", "application/json")
-			}
-		}
+		utils.JSONError(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	user, err := s.SupabaseFactory.CreateClient().Auth.Signup(types.SignupRequest{
+		Email:    registerRequest.Email,
+		Password: registerRequest.Password,
+	})
+	if err != nil {
+		utils.JSONError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	profile := t.Profile{
+		ID:       user.ID,
+		Username: registerRequest.Username,
+	}
+	_, _, err = s.SupabaseFactory.CreateAdminClient().From("profile").Insert(profile, true, "", "", "exact").Execute()
+	if err != nil {
+		del_err := s.SupabaseFactory.CreateAdminClient().Auth.AdminDeleteUser(types.AdminDeleteUserRequest{UserID: user.ID})
+		if del_err != nil {
+			utils.JSONError(w, del_err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		utils.JSONError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resp["email"] = user.Email
+	w.Header().Set("Content-Type", "application/json")
 
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
