ba46c9b22a9496a0f579265edb595954666f57bb
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-12-28 15:43:30 +0100

    Add create endpoint

--- backend/internal/server/collection.go
+++ backend/internal/server/collection.go
@@ -6,9 +6,15 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
+
+type CreateCollectionRequest struct {
+	Name string `json:"name"`
+}
 
 func (s *Server) ListCollections(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]any)
@@ -54,6 +60,51 @@ func (s *Server) GetCollection(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(jsonResp)
 }
 
+func (s *Server) CreateCollection(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]any)
+
+	var createCollectionRequest CreateCollectionRequest
+	err := json.NewDecoder(r.Body).Decode(&createCollectionRequest)
+	if err != nil {
+		utils.JSONError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	token := r.Context().Value(types.JwtTokenKey).(string)
+	userID := r.Context().Value(types.UserID).(string)
+	user_client := s.SupabaseFactory.CreateAuthenticatedClient(token)
+
+	collection := types.Collection{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UserID:    uuid.MustParse(userID),
+		Name:      createCollectionRequest.Name,
+	}
+	_, _, err = user_client.From("collection").Insert(collection, false, "", "", "exact").Execute()
+	if err != nil {
+		utils.JSONError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	resp["name"] = createCollectionRequest.Name
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) UpdateCollection(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) DeleteCollection(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func (s *Server) CollectionCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
@@ -83,9 +134,12 @@ func (s *Server) CollectionCtx(next http.Handler) http.Handler {
 func CollectionRouter(s *Server) chi.Router {
 	r := chi.NewRouter()
 	r.Get("/", s.ListCollections)
+	r.Post("/", s.CreateCollection)
 	r.Route("/{id}", func(r chi.Router) {
 		r.Use(s.CollectionCtx)
 		r.Get("/", s.GetCollection)
+		r.Delete("/", s.DeleteCollection)
+		r.Put("/", s.UpdateCollection)
 	})
 	return r
 }
