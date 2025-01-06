481df8999aaf2dc6cec7fa2b51db56c3077c8cba
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2025-01-06 16:54:28 +0100

    Working on supabase function

--- supabase/functions/generateHashes/index.ts
+++ supabase/functions/generateHashes/index.ts
@@ -1,12 +1,13 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { getCards } from "https://x.nest.land/pokedeno@0.2.1/mod.ts";
 
 Deno.serve(async (req) => {
-  const { name } = await req.json();
-  const data = {
-    message: `Hello ${name}!`,
-  };
+  const TCG_API_KEY = Deno.env.get("TCG_API_KEY");
 
-  return new Response(JSON.stringify(data), {
+  const cards = await getCards({});
+  const ids = cards.map((card) => card.id);
+
+  return new Response(JSON.stringify({ ids }), {
     headers: { "Content-Type": "application/json" },
   });
 });
