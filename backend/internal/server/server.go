fbd2a563c06f32f46eea051b9324322fa9775593
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2025-01-10 10:48:28 +0100

    Download hashes from supabase

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
@@ -24,6 +25,31 @@ type Server struct {
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	supabaseFactory := sb.NewSupabaseFactory()
+
+	// Download card_hashes.gob from Supabase storage
+	client := supabaseFactory.CreateAdminClient()
+	url, err := client.Storage.CreateSignedUrl("hashes", "card_hashes.gob", 60)
+	if err != nil {
+		fmt.Printf("Error creating signed url: %v\n", err)
+	}
+	resp, err := http.Get(url.SignedURL)
+	if err != nil {
+		fmt.Printf("Error downloading card hashes: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	// Create local file
+	out, err := os.OpenFile("./data/card_hashes.gob", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		fmt.Printf("Error creating local file: %v\n", err)
+	}
+	defer out.Close()
+
+	// Copy downloaded content to local file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Printf("Error copying content to file: %v\n", err)
+	}
 
 	NewServer := &Server{
 		SupabaseFactory: supabaseFactory,
