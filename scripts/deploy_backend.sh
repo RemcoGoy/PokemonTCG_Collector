3f592bb81d8be84acee3e1dfba5386e90b6e823d
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2025-01-10 12:28:21 +0100

    Fix deployment

--- scripts/deploy_backend.sh
+++ scripts/deploy_backend.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cd backend && gcloud run deploy backend --source . --region europe-west1 --platform managed --allow-unauthenticated
+cd backend
+docker build --platform linux/amd64 -t tcg-backend:latest .
+gcloud auth configure-docker europe-west1-docker.pkg.dev
+docker tag tcg-backend:latest europe-west1-docker.pkg.dev/pokemontcg-446314/tcg-backend/tcg-backend:latest
+docker push europe-west1-docker.pkg.dev/pokemontcg-446314/tcg-backend/tcg-backend:latest
+
+gcloud run deploy backend --image europe-west1-docker.pkg.dev/pokemontcg-446314/tcg-backend/tcg-backend:latest --platform managed --region europe-west1 --allow-unauthenticated --port 3000 --env-vars-file .env.yaml
